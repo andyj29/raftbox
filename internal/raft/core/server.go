50c2d715bcb33935b0911a80c24becb5d1426d63
andyj29 <andycryptotrade@gmail.com>
2023-01-14 17:40:09 -0500

    add some raft server methods

--- internal/raft/core/server.go
+++ internal/raft/core/server.go
@@ -17,6 +17,9 @@ const (
 	FOLLOWER
 )
 
+// Server is a struct for a Raft server. It contains the necessary fields for maintaining the
+// state of the server, implementing the Raft consensus algorithm, and communicating with
+// other servers in the cluster.
 type Server struct {
 	mu        sync.Mutex
 	peers     []*rpc.Client
@@ -28,7 +31,7 @@ type Server struct {
 	votedFor    int
 	log         []LogEntry
 
-	newCond     sync.Cond
+	newCond     *sync.Cond
 	commitIndex int
 	lastApplied int
 	applyChan   chan<- ApplyMsg
@@ -43,40 +46,98 @@ func NewRaftServer(
 	persister *storage.Persister,
 	applyChan chan<- ApplyMsg,
 ) *Server {
+	mu := sync.Mutex{}
 	rs := &Server{
+		mu:          mu,
 		peers:       peers,
 		storage:     persister,
 		state:       FOLLOWER,
 		selfIndex:   selfIndex,
 		currentTerm: 0,
 		votedFor:    -1,
+		newCond:     sync.NewCond(&mu),
 		commitIndex: 0,
 		lastApplied: 0,
 		applyChan:   applyChan,
 	}
 
-	// re-initialize from pre-crash state
-	rs.initState(persister.ReadState())
+	// re-initialize non-volatile state from pre-crash
+	rs.initNonVolatileState(persister.ReadState())
+	rs.applySnapshot(persister.ReadSnapshot())
 	return rs
 }
 
-func (rs *Server) initState(data []byte) {
+func (rs *Server) initNonVolatileState(data []byte) {
 	if len(data) == 0 {
 		return
 	}
 
-	raw := bytes.NewBuffer(data)
-	decoder := gob.NewDecoder(raw)
+	r := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(r)
 	var (
 		currentTerm, votedFor int
-		logs                  []LogEntry
+		log                   []LogEntry
 	)
 	if decoder.Decode(&currentTerm) != nil ||
 		decoder.Decode(&votedFor) != nil ||
-		decoder.Decode(&logs) != nil {
+		decoder.Decode(&log) != nil {
 		return
 	}
 	rs.currentTerm = currentTerm
 	rs.votedFor = votedFor
-	rs.log = logs
+	rs.log = log
+}
+
+func (rs *Server) getLastLogIndex() int {
+	return rs.log[len(rs.log)-1].Index
+}
+
+func (rs *Server) getLastLogTerm() int {
+	return rs.log[len(rs.log)-1].Term
+}
+
+func (rs *Server) getNonVolatileState() []byte {
+	w := new(bytes.Buffer)
+	encoder := gob.NewEncoder(w)
+
+	if encoder.Encode(rs.currentTerm) != nil ||
+		encoder.Encode(rs.votedFor) != nil ||
+		encoder.Encode(rs.log) != nil {
+		return nil
+	}
+	return w.Bytes()
+}
+
+func (rs *Server) applySnapshot(snapshot []byte) {
+	if snapshot == nil || len(snapshot) == 0 {
+		return
+	}
+
+	var lastIncludedIndex, lastIncludedTerm int
+	r := bytes.NewBuffer(snapshot)
+	decoder := gob.NewDecoder(r)
+	decoder.Decode(&lastIncludedIndex)
+	decoder.Decode(&lastIncludedTerm)
+
+	rs.lastApplied = lastIncludedIndex
+	rs.commitIndex = lastIncludedIndex
+
+	rs.trimLog(lastIncludedIndex, lastIncludedTerm)
+}
+
+func (rs *Server) trimLog(lastIncludedIndex, lastIncludedTerm int) {
+	newLog := make([]LogEntry, 0)
+	newLog = append(newLog, LogEntry{Index: lastIncludedIndex, Term: lastIncludedTerm})
+
+	for i := len(rs.log) - 1; i >= 0; i-- {
+		if rs.log[i].Index == lastIncludedIndex && rs.log[i].Term == lastIncludedTerm {
+			newLog = append(newLog, rs.log[i+1:]...)
+			break
+		}
+
+	}
+	rs.log = newLog
+}
+func (rs *Server) saveState() {
+	rs.storage.SaveState(rs.getNonVolatileState())
 }
--- internal/raft/storage/storage.go
+++ internal/raft/storage/storage.go
@@ -6,3 +6,11 @@ type Persister struct {
 func (p *Persister) ReadState() []byte {
 	return nil
 }
+
+func (p *Persister) SaveState(data []byte) {
+
+}
+
+func (p *Persister) ReadSnapshot() []byte {
+	return nil
+}
